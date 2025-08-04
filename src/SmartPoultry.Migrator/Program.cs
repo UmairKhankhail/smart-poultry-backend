921089635472f4fdd8f05ecebda92c3f6c6ea1a8
Umair Khan <umairkhankail@gmail.com>
2025-08-05 00:59:29 +0500

    program.cs changed to manage migrator does not run.

--- src/SmartPoultry.Migrator/Program.cs
+++ src/SmartPoultry.Migrator/Program.cs
@@ -15,32 +15,32 @@ namespace SmartPoultry.Migrator
         {
             ParseArgs(args);
 
-            using (var bootstrapper = AbpBootstrapper.Create<SmartPoultryMigratorModule>())
-            {
-                bootstrapper.IocManager.IocContainer
-                    .AddFacility<LoggingFacility>(
-                        f => f.UseAbpLog4Net().WithConfig("log4net.config")
-                    );
+            //using (var bootstrapper = AbpBootstrapper.Create<SmartPoultryMigratorModule>())
+            //{
+            //    bootstrapper.IocManager.IocContainer
+            //        .AddFacility<LoggingFacility>(
+            //            f => f.UseAbpLog4Net().WithConfig("log4net.config")
+            //        );
 
-                bootstrapper.Initialize();
+            //    bootstrapper.Initialize();
 
-                using (var migrateExecuter = bootstrapper.IocManager.ResolveAsDisposable<MultiTenantMigrateExecuter>())
-                {
-                    var migrationSucceeded = migrateExecuter.Object.Run(_quietMode);
+            //    using (var migrateExecuter = bootstrapper.IocManager.ResolveAsDisposable<MultiTenantMigrateExecuter>())
+            //    {
+            //        var migrationSucceeded = migrateExecuter.Object.Run(_quietMode);
                     
-                    if (_quietMode)
-                    {
-                        // exit clean (with exit code 0) if migration is a success, otherwise exit with code 1
-                        var exitCode = Convert.ToInt32(!migrationSucceeded);
-                        Environment.Exit(exitCode);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Press ENTER to exit...");
-                        Console.ReadLine();
-                    }
-                }
-            }
+            //        if (_quietMode)
+            //        {
+            //            // exit clean (with exit code 0) if migration is a success, otherwise exit with code 1
+            //            var exitCode = Convert.ToInt32(!migrationSucceeded);
+            //            Environment.Exit(exitCode);
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Press ENTER to exit...");
+            //            Console.ReadLine();
+            //        }
+            //    }
+            //}
         }
 
         private static void ParseArgs(string[] args)
